82f1cc1645f37b2b33d478dda305d786dd5d2296
GabrielZilmar <gabrielzilmar@hotmail.com>
2020-07-15 10:54:47 -0300

    [+] botao init

---
+++ botao/src/Components/Contador/Contador.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contador = () => {
+	return (
+		<div>
+			<h1>Hello World!</h1>
+			<h2>Let's learn React.js</h2>
+		</div>
+	);
+};
+
+export default Contador;
---
+++ botao/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import Contador from "./Components/Contador/Contador.jsx";
+
+ReactDOM.render(<Contador></Contador>, document.getElementById("root"));

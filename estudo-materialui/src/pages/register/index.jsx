e03de39fae0dceed4c7fb51aba398dd3d313dc77
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2021-05-28 19:19:33 -0300

    feat: resposiveness register screen

--- estudo-materialui/src/pages/register/index.jsx
+++ estudo-materialui/src/pages/register/index.jsx
@@ -26,9 +26,6 @@ const styles = (theme)=>({
   paper: {
     background: '#fff',
     borderRadius: 10,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
     color: '#000',
     padding: 12,
     boxShadow: '6px 6px rgba(0, 0, 0, 0.2)',
@@ -46,11 +43,6 @@ const styles = (theme)=>({
   },
   container: {
     'margin': 0,
-    'position': 'absolute',
-    'top': '50%',
-    'left': '50%',
-    '-ms-transform': 'translate(-50%, -50%)',
-    'transform': 'translate(-50%, -50%)',
   },
 });
 
@@ -66,144 +58,155 @@ class Register extends React.Component {
     const {classes} = this.props;
 
     return (
-      <Container component="main" maxWidth="md" className={classes.container}>
-        <div className={classes.paper}>
-          <Box
-            display="flex"
-            alignItems="center"
-            bgcolor="background.paper"
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+        minHeight={690}
+        minWidth={350}
+      >
+        <Box >
+          <Container
+            component="main"
+            maxWidth="md"
+            className={classes.container}
           >
-            <Box order={1} width="95%">
-              <Grid container >
-                <Grid item xs={12}>
-                  <Avatar
-                    className={classes.avatar}
-                    style={{marginTop: 20}}
-                  >
-                    <AccountCircleIcon />
-                  </Avatar>
-                  <Typography
-                    component="h1"
-                    variant="h5"
-                    align="center"
-                    style={{marginTop: 6}}
-                  >
+            <div className={classes.paper}>
+              <Box
+                display="flex"
+                alignItems="center"
+                bgcolor="background.paper"
+              >
+                <Box order={1}>
+                  <Grid container >
+                    <Grid item xs={12}>
+                      <Avatar
+                        className={classes.avatar}
+                        style={{marginTop: 20}}
+                      >
+                        <AccountCircleIcon />
+                      </Avatar>
+                      <Typography
+                        component="h1"
+                        variant="h5"
+                        align="center"
+                        style={{marginTop: 6}}
+                      >
                 Register
-                  </Typography>
-                  <FormControl className={classes.form}>
-                    <Grid container spacing={2} >
-                      <Grid item xs={6}>
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="firstName"
-                          name="name"
-                          label="First Name"
-                          autoFocus
-                        />
-                      </Grid>
-                      <Grid item xs={6}>
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="lastName"
-                          name="name"
-                          label="Last Name"
-                          autoFocus
-                        />
-                      </Grid>
-                      <Grid item xs={12}>
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="email"
-                          name="email"
-                          label="Email Address"
-                          autoComplete="email"
-                          autoFocus
-                        />
-                      </Grid>
-                      <Grid item xs={6}>
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="password"
-                          name="password"
-                          label="Password"
-                          type="password"
-                          autoComplete="current-password"
-                        />
-                      </Grid>
-                      <Grid item xs={6}>
-                        <TextField
-                          variant="outlined"
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="confirm"
-                          name="password"
-                          label="Confirm"
-                          type="password"
-                          autoComplete="current-password"
-                        />
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox value="showPass" color="primary" />
-                        }
-                        label="Show password"
-                      />
-                    </Grid>
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      className={classes.submit}
-                    >
-            Register
-                    </Button>
-                    <Grid container justify="flex-end">
-                      <Grid item xs style={{textAlign: 'end'}}>
-                        <Link href="/" variant="body2">
-                          {'Do have an account? Sign In'}
-                        </Link>
-                      </Grid>
-                    </Grid>
-                  </FormControl>
-                </Grid>
+                      </Typography>
+                      <FormControl className={classes.form}>
+                        <Grid container spacing={2} >
+                          <Grid item xs={6}>
+                            <TextField
+                              variant="outlined"
+                              margin="normal"
+                              required
+                              fullWidth
+                              id="firstName"
+                              name="name"
+                              label="First Name"
+                              autoFocus
+                            />
+                          </Grid>
+                          <Grid item xs={6}>
+                            <TextField
+                              variant="outlined"
+                              margin="normal"
+                              required
+                              fullWidth
+                              id="lastName"
+                              name="name"
+                              label="Last Name"
+                            />
+                          </Grid>
+                          <Grid item xs={12}>
+                            <TextField
+                              variant="outlined"
+                              margin="normal"
+                              required
+                              fullWidth
+                              id="email"
+                              name="email"
+                              label="Email Address"
+                              autoComplete="email"
+                            />
+                          </Grid>
+                          <Grid item xs={6}>
+                            <TextField
+                              variant="outlined"
+                              margin="normal"
+                              required
+                              fullWidth
+                              id="password"
+                              name="password"
+                              label="Password"
+                              type="password"
+                            />
+                          </Grid>
+                          <Grid item xs={6}>
+                            <TextField
+                              variant="outlined"
+                              margin="normal"
+                              required
+                              fullWidth
+                              id="confirm"
+                              name="password"
+                              label="Confirm"
+                              type="password"
+                            />
+                          </Grid>
 
-              </Grid>
-            </Box>
-            <Box order={2} >
-              <CardMedia
-                component="img"
-                alt="Register Image"
-                image={RegisterImg}
-                style={
-                  {
-                    'margin': '0 auto',
-                    'width': '80%',
-                  }
-                }
-              />
-            </Box>
-          </Box>
-          <Box mt={8}>
-            <Copyright />
-          </Box>
-        </div>
-      </Container>
+                          <Grid item xs={6}>
+                            <FormControlLabel
+                              control={
+                                <Checkbox value="showPass" color="primary" />
+                              }
+                              label="Show password"
+                            />
+                          </Grid>
+                          <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                          >
+            Register
+                          </Button>
+                        </Grid>
+                        <Grid container justify="flex-end">
+                          <Grid item xs style={{textAlign: 'end'}}>
+                            <Link href="/" variant="body2">
+                              {'Do have an account? Sign In'}
+                            </Link>
+                          </Grid>
+                        </Grid>
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Box>
+                <Box order={2} >
+                  <CardMedia
+                    component="img"
+                    alt="Register Image"
+                    image={RegisterImg}
+                    style={
+                      {
+                        'margin': '0 auto',
+                        'width': '80%',
+                      }
+                    }
+                  />
+                </Box>
+              </Box>
+              <Box mt={8}>
+                <Copyright />
+              </Box>
+            </div>
+          </Container>
+        </Box>
+      </Box>
     );
   }
 }
